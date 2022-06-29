ae34e0b8674ba2437ce92de587522f4bd4893517
lastrise <54949052+lastrise@users.noreply.github.com>
2022-06-29 04:17:36 +0300

    fixed assert

--- test_items.py
+++ test_items.py
@@ -6,5 +6,5 @@ class TestShopItems:
     @pytest.mark.parametrize("url", ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/", ])
     def test_basket_button_exists(self, browser, url: str):
         browser.get(url)
-        browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket")
-
+        buttons = browser.find_elements(By.CSS_SELECTOR, "button.btn-add-to-basket123123")
+        assert len(buttons) == 1
